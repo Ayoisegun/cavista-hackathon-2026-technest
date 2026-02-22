d98aae40ace3409c15e3cbfa2dff7504dc21d0e1
Deezaya <omodesire88@gmail.com>
2026-02-22 09:26:25 +0100

    Update database engine configuration for SQLite

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from sqlmodel import SQLModel, create_engine
+import os
 
 DATABASE_URL = "sqlite:///eldercare.db"
 
-engine = create_engine(DATABASE_URL, echo=True)
-
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    connect_args={"check_same_thread": False}
+)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
